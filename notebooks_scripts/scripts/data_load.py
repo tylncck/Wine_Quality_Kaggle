c593969ae77d3c8d4b1f32b76e685a552552d073
tylncck <cicek.onurtaylan@gmail.com>
2023-06-12 01:55:37 -0600

    new features added to data_load_py and EDA updated

--- notebooks_scripts/scripts/data_load.py
+++ notebooks_scripts/scripts/data_load.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def data_reading():
     train_df = pd.read_csv('../../data/train.csv')
@@ -10,14 +11,27 @@ def data_reading():
     return train_df, test_df
 
 def feature_generation(df):
+    
     #Total Acidity
-    df['Total_Acidity'] = df['fixed_acidity'] + df['volatile_acidity']
-    #Fixed Acidity Ratio
-    df['Fixed_Acidity_Ratio'] = df['fixed_acidity'] / df['Total_Acidity']
-    #Sugar to Acidity Ratio
-    df['Sugar_Acidity_Ratio'] = df['residual_sugar'] / df['Total_Acidity']
-    #Free-Sulphure_Dioxide / Total Sulphure Diocxide
-    df['FreeSulphure_Ratio'] = df['free_sulfur_dioxide'] / df['total_sulfur_dioxide']
-    #Alcohol x  pH
-    df['Alcohol_x_pH'] = df['alcohol'] * df['pH']
+    df['total_acidity'] = df['fixed_acidity'] + df['volatile_acidity'] + df['citric_acid']
+    
+    columns = list(df.columns)
+
+    for i in range(len(columns)):
+        for j in range(i+1, len(columns)):
+            if columns[i] != 'quality' and columns[j] != 'quality':
+                col_name1 = columns[i] + '_' + columns[j] + '_r'
+                col_name2 = columns[i] + '_' + columns[j] + '_m'
+                df[col_name1] = df[columns[i]] / df[columns[j]]
+                df[col_name2] = df[columns[i]] * df[columns[j]]
+                df[col_name1] = df[col_name1].replace([np.inf, -np.inf], np.nan)
+    
+    # MolecularSO2
+    df['molecularso2'] = df['free_sulfur_dioxide']/(1+ 10**(df['pH'] -1.81))
+
+    df['alcohol_density_r_totalacidity_r'] = (1 / df['density_alcohol_r']) / df['total_acidity']
+    df['total_acidity_sulphates_p'] = df['total_acidity'] + df['sulphates']
+    df['density_alcohol_r_sulphates_chlorides_r'] = df['density_alcohol_r'] * 1/df['chlorides_sulphates_r']
+    df['total_acidity_alcohol_r+sulphates*alcohol+total_acidity'] = (1 / df['alcohol_total_acidity_r']) + df['sulphates_alcohol_m'] + df['total_acidity']
+    
     return df
